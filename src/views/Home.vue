6921523e715a2f2904e7652f030ec90c2a140e54
liaowang <1245208481@qq.com>
2020-03-23 15:10:58 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部 -->
+    <el-header>
+      <div>
+        <img src="../assets/logo.png" />
+        <span>音乐</span>
+      </div>
+    </el-header>
+    <!-- 主体区 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <!-- 侧边导航 -->
+      </el-aside>
+      <!-- 右侧内容 -->
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+export default {}
+</script>
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+<style lang="less" scoped>
+.el-header{
+  img{
+    height: 30px
   }
 }
-</script>
+/* .el-header img {
+  height: 30px;
+}
+.el-header span{
+  height: 30px;
+  line-height: 30px;
+  text-align: center
+} */
+</style>
